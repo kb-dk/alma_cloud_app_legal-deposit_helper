8b474943a305d0c940d85b4f344e322acec81e1e
Jørgen Jegsen <jjeg@kb.dk>
2021-12-15 15:03:22 +0100

    Delete and update works. Code is a mess :-)

--- cloudapp/src/app/poline/poline.component.ts
+++ cloudapp/src/app/poline/poline.component.ts
@@ -99,8 +99,29 @@ export class POlineComponent implements OnInit, OnDestroy {
     });
   }
 
+  private sendPoLineUpdateRequest(link: string, requestBody: any) {
+    let request: Request = {
+      url: link,
+      method: HttpMethod.PUT,
+      requestBody
+    };
+    this.restService.call(request).subscribe({
+      next: result => {
+        this.alert.success("PO-line updated")
+        this.apiResult = result;
+        this.refreshPage();
+      },
+      error: (e: RestErrorResponse) => {
+        this.alert.error('Failed to update data');
+        console.error(e);
+        this.loading = false;
+      }
+    });
+  }
+
   private sendGetRequest(entity: Entity) {
     //TODO: work in progress
+    console.log("sendGetRequest -> Link: " + entity.link);
     let url = entity.link;
     let request: Request = {
       url: url,
@@ -108,10 +129,11 @@ export class POlineComponent implements OnInit, OnDestroy {
     };
     this.restService.call(request).subscribe({
       next: result => {
-        let jsonResultAsString =  JSON.stringify(result);
-        let parsedToJSON = JSON.parse(jsonResultAsString);
-        this.alert.success("Vendor: " + parsedToJSON.vendor.value + ",  "+ parsedToJSON.vendor.desc);
-        this.apiResult = result;
+        if(url.includes("po-lines")){
+          this.poLineStuff(result);
+        } else{
+          this.apiResult = result;
+        }
         // this.refreshPage();
       },
       error: (e: RestErrorResponse) => {
@@ -122,28 +144,222 @@ export class POlineComponent implements OnInit, OnDestroy {
     });
   }
 
+  private sendGetRequestFromLink(link: string) {
+    //TODO: work in progress
+    console.log("sendGetRequest -> Link: " + link);
+    let request: Request = {
+      url: link,
+      method: HttpMethod.GET
+    };
+    this.restService.call(request).subscribe({
+      next: result => {
+        console.log("bibData fundet")
+          this.apiResult = result;
+        // this.refreshPage();
+      },
+      error: (e: RestErrorResponse) => {
+        this.alert.error('Failed to get data from ' + link);
+        console.error(e);
+        this.loading = false;
+      },
+      complete: () => {
+        this.alert.error('Data OK from ' + link);
+      }
+    });
+  }
+
+  private sendTestPoLineUpdate(link: string) {
+    //TODO: work in progress
+    console.log("sendGetRequest -> Link: " + link);
+    let request: Request = {
+      url: link,
+      method: HttpMethod.GET
+    };
+    this.restService.call(request).subscribe({
+      next: result => {
+        let jsonResultAsString = JSON.stringify(result);
+        var parsedToJSON = JSON.parse(jsonResultAsString);
+        var vendorMessage = "Vendor: " + parsedToJSON.vendor.value + ",  " + parsedToJSON.vendor.desc + "  Account: " + parsedToJSON.vendor_account;
+        console.log("FØR: " +vendorMessage);
+        parsedToJSON.vendor.value = "1AAKS";
+        parsedToJSON.vendor.desc = "Aalborg Kommune";
+        parsedToJSON.vendor_account = "1AAKS";
+        vendorMessage = "Vendor: " + parsedToJSON.vendor.value + ",  " + parsedToJSON.vendor.desc + "  Account: " + parsedToJSON.vendor_account;
+        console.log("EFTER: " +vendorMessage);
+        let updatedJson = JSON.stringify(parsedToJSON);
+        console.log('updatedJson: ' +updatedJson);
+        this.update(updatedJson);//update parser selv til JSON...
+        this.refreshPage();
+      },
+      error: (e: RestErrorResponse) => {
+        this.alert.error('Failed to get data from ' + link);
+        console.error(e);
+        this.loading = false;
+      },
+      complete: () => {
+        this.alert.error('Data OK from ' + link);
+      }
+    });
+  }
+
+/*
+  private sendTestPoLineDelete(link: string) {
+    //TODO: work in progress
+    console.log("sendGetRequest -> Link: " + link);
+    let request: Request = {
+      url: link,
+      method: HttpMethod.GET
+    };
+    this.restService.call(request).subscribe({
+      next: result => {
+        let jsonResultAsString = JSON.stringify(result);
+        var parsedToJSON = JSON.parse(jsonResultAsString);
+        this.sendDeleteRequestString("");//update parser selv til JSON...
+        this.refreshPage();
+      },
+      error: (e: RestErrorResponse) => {
+        this.alert.error('Failed to get data from ' + link);
+        console.error(e);
+        this.loading = false;
+      },
+      complete: () => {
+        this.alert.error('Data OK from ' + link);
+      }
+    });
+  }
+*/
+
+  private poLineStuff(result) {
+    let jsonResultAsString = JSON.stringify(result);
+    let parsedToJSON = JSON.parse(jsonResultAsString);
+    let mmsId = parsedToJSON.resource_metadata.mms_id.value;//TODO: Bruges til at gafle felt 280b
+    let linkToBibPost = parsedToJSON.resource_metadata.mms_id.link;//TODO: Bruges til at gafle felt 280b
+    let mmsMessage = "MMS: " + mmsId + "  link: " + linkToBibPost;
+    this.alert.success(mmsMessage);
+    console.log(mmsMessage);
+    let vendorMessage = "Vendor: " + parsedToJSON.vendor.value + ",  " + parsedToJSON.vendor.desc;
+    this.alert.success(vendorMessage);
+    this.sendGetRequestFromLink(linkToBibPost)
+    // this.apiResult = result;
+  }
+
   private sendDeleteRequest(entity: Entity) {
     let poLineString = "" + entity.description + ", Id: " + entity.id;
+    console.log(poLineString);
     let id = entity.id;
-    let url = "/acp/po-lines/" + entity.id;
+    let url = "/acq/po-lines/" + entity.id;
     let request: Request = {
       url: url,
       method: HttpMethod.DELETE,
       queryParams: {["reason"]:"LIBRARY_CANCELLED"}
     };
-    console.log("Request: " + request);//DELETE http://localhost:4200/almaws/v1/acp/po-lines/22993180260005763?reason=LIBRARY_CANCELLED&_=1639464026197 TODO:Why this: "&_=1639464026197 "
-    this.restService.call(request).subscribe({ //TODO: FAILS
+    console.log("Request: " + request);
+    this.restService.call(request).subscribe({
       next: result => {
         this.deletedEntities.push(id);
-        this.alert.success("Vendor is deleted: " + poLineString, { autoClose: false });
+        this.alert.success("PO-line is deleted: " + poLineString, { autoClose: false });
         this.refreshPage();
       },
       error: (e: RestErrorResponse) => {
-        this.alert.error('Failed to delete vendor: ' + poLineString);
+        this.alert.error('Failed to delete PO-line: ' + poLineString);
         console.error(e);
         this.loading = false;
+      },
+      complete: () => {
+        this.alert.success("PO-lines deleted: " + this.deletedEntities.length + " Id's are: " + this.deletedEntities);
       }
     });
+  }
+  private RENAMEsendDeleteRequestString(entity: Entity ) {
+    //TODO: work in progress
+    console.log("sendGetRequest -> Link: " + entity.link);
+    var request: Request = {
+      url: entity.link,
+      method: HttpMethod.GET
+    };
+    this.restService.call(request).subscribe({
+      next: result => {
+        console.log("po-line data fundet")
+        this.apiResult = result;
+        let jsonResultAsString = JSON.stringify(result);
+        let parsedToJSON = JSON.parse(jsonResultAsString);
+        let poLineCode = parsedToJSON.number;
+        let url = "/acq/po-lines/" + poLineCode;
+        var deleteRequest: Request = {
+          url: url,
+          method: HttpMethod.DELETE,
+          queryParams: {["reason"]:"LIBRARY_CANCELLED"}
+        };
+        console.log("DeleteUrl: " + url);
+        this.restService.call(deleteRequest).subscribe({
+          next: () => {
+            this.deletedEntities.push(poLineCode);
+            this.alert.success("PO-line is deleted: " + poLineCode, { autoClose: false });
+            this.refreshPage();
+          },
+          error: (e: RestErrorResponse) => {
+            this.alert.error('Failed to delete PO-line: ' + poLineCode);
+            console.error(e);
+            this.loading = false;
+          },
+          complete: () => {
+            this.alert.success("PO-lines deleted: " + this.deletedEntities.length + " Id's are: " + this.deletedEntities);
+          }
+        });
+
+
+
+        /*
+                let url = "/acq/po-lines/" + entityId;
+                let request2: Request = {
+                  url: url,
+                  method: HttpMethod.DELETE,
+                  queryParams: {["reason"]:"LIBRARY_CANCELLED"}
+                };
+                console.log("DeleteUrl: " + url);
+                this.restService.call(request).subscribe({
+                  next: result => {
+                    this.deletedEntities.push(entityId);
+                    this.alert.success("PO-line is deleted: " + entityId, { autoClose: false });
+                    this.refreshPage();
+                  },
+                  error: (e: RestErrorResponse) => {
+                    this.alert.error('Failed to delete PO-line: ' + entityId);
+                    console.error(e);
+                    this.loading = false;
+                  },
+                  complete: () => {
+                    this.alert.success("PO-lines deleted: " + this.deletedEntities.length + " Id's are: " + this.deletedEntities);
+                  }
+                });
+        */
+
+
+
+
+
+
+
+        // this.refreshPage();
+      },
+      error: (e: RestErrorResponse) => {
+        this.alert.error('Failed to get data from ' + entity.link);
+        console.error(e);
+        this.loading = false;
+      },
+      complete: () => {
+        this.alert.error('Data OK from ' + entity.link);
+      }
+    });
+
+
+
+
+
+
+
+
+
   }
 
   private tryParseJson(value: any) {
@@ -166,10 +382,15 @@ export class POlineComponent implements OnInit, OnDestroy {
   DeleteSelected() {
     this.deletedEntities = new Array<String>();
     this.selectedEntities.forEach(entity => {
-      this.sendDeleteRequest(entity);
+      this.RENAMEsendDeleteRequestString(entity);
     })
-    this.alert.success("Id's deleted: " + this.deletedEntities);
   }
 
+  SetNewVendor() {
+    this.selectedEntities.forEach(entity => {
+      let link = entity.link;
+      this.sendTestPoLineUpdate(link);
+    })
+  }
 
 }
